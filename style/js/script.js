d24017c9c752f0d3252d631d00c4e668f8e0ce6c
beramsey <beramsey@gmail.com>
2023-03-28 14:30:10 -0400

    Chart

--- style/js/script.js
+++ style/js/script.js
@@ -1,15 +1,14 @@
 // Popularity Line Chart
-const years = [2017,2018,2019,2020,2021,2022,2023];
-const players = [30,78.3,75.5,80.4,83.3,85,90];
 
 new Chart("popChart", {
   type: "line",
   data: {
-    labels: years,
+    labels: ['2017','2018','2019','2020','2021','2022','2023'],
     datasets: [{
-      backgroundColor:"#ffffff",
+      backgroundColor:"black",
       borderColor: "#f3af19",
-      data: players
+      label: 'Popularity Over Time',
+      data: [30,78.3,75.5,80.4,83.3,85,90],
     }]
   },
   options: {
@@ -24,22 +23,3 @@ new Chart("popChart", {
 });
 
 // Fortnite Skins API
-const axios = require("axios");
-
-const options = {
-  method: 'POST',
-  url: 'https://fortniteskins.p.rapidapi.com/',
-  params: {Name: 'Jonsey', Price: '800', Rarity: 'Epic', Source: 'Shop'},
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': 'ffed1289ffmsh83cfed6f9f2a410p15dd43jsna9795d185233',
-    'X-RapidAPI-Host': 'fortniteskins.p.rapidapi.com'
-  },
-  data: '{"key1":"value","key2":"value"}'
-};
-
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
