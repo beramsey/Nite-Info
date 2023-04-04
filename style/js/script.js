d395e6679afb318a43d494c906611f7678f18274
beramsey <beramsey@gmail.com>
2023-04-03 21:15:01 -0400

    added api

--- style/js/script.js
+++ style/js/script.js
@@ -1,11 +1,46 @@
-// Popularity Line Chart
+// Youtube API
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ffed1289ffmsh83cfed6f9f2a410p15dd43jsna9795d185233',
+		'X-RapidAPI-Host': 'youtube-search-results.p.rapidapi.com'
+	}
+};
 
+const userInput = document.querySelector('input');
+const btn = document.querySelector('button');
+
+let params = ''
+const callParams = () => {
+    params = userInput.value
+     fetch(`https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${params}?_limit=5`, options)
+        .then(response => response.json())
+        .then(data => {
+            let output = '';
+            data.items.map(item => {
+                output += `
+                    <h1>${item.title}</h1>
+                    <li>
+                    <a href="${item.url}">${item.url}</a>
+                    </li>
+                    `;
+                  
+            })
+            document.querySelector('.result').innerHTML = output;
+        })
+        .catch(err => console.error(err));
+    userInput.value = '';
+}
+
+
+btn.addEventListener('click', callParams)
+
+// Popularity Line Chart
 new Chart("popChart", {
   type: "line",
   data: {
     labels: ['2017','2018','2019','2020','2021','2022','2023'],
     datasets: [{
-      backgroundColor:"white",
       borderColor: "#f3af19",
       label: 'Popularity Over Time',
       data: [30,78.3,75.5,80.4,83.3,85,90],
@@ -23,7 +58,6 @@ new Chart("popChart", {
 });
 
 // Fortnite Character Generator
-
 const characterArray = [
   "style/images/characters/fortnite-outfit-Leon-S-Kennedy.webp",
   "style/images/characters/fortnite-outfit-Claire-Redfield.webp",
@@ -43,21 +77,6 @@ window.onload = () => generateRandomPicture(characterArray);
 button.addEventListener("click", () => generateRandomPicture(characterArray));
 
 function generateRandomPicture(array){
-	let randomNum = Math.floor(Math.random() * array.length); 
-	image.setAttribute("src", array[randomNum]);
+    let randomNum = Math.floor(Math.random() * array.length); 
+    image.setAttribute("src", array[randomNum]);
 }
-
-// Fortnite API
-const options = {
-	method: 'GET',
-	headers: {
-		Type: 'br',
-		'X-RapidAPI-Key': 'ffed1289ffmsh83cfed6f9f2a410p15dd43jsna9795d185233',
-		'X-RapidAPI-Host': 'fortnite4.p.rapidapi.com'
-	}
-};
-
-fetch('https://fortnite4.p.rapidapi.com/news', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
